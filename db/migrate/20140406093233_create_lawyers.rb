af703675026ce16ca7f8d16012f5f5f2e8e9c4cc
Anush Veeranala <anush.2k10@gmail.com>
2014-04-06 15:15:39 +0530

    coded lawyer migration'

--- db/migrate/20140406093233_create_lawyers.rb
+++ db/migrate/20140406093233_create_lawyers.rb
@@ -1,8 +1,17 @@
 class CreateLawyers < ActiveRecord::Migration
   def change
     create_table :lawyers do |t|
-
+      t.belongs_to :city, null: false
+      t.string :code, null: false, limit: 250
+      t.string :name, null: false, limit: 250
+      t.integer :experience, null: false, default: 0
+      t.float :rating, null: false, default: 0
+      t.charges_count, null: false, default: 0
       t.timestamps
     end
+    add_index :lawyers, :city_id
+    add_index :lawyers, :code, unique: true
+    add_index :lawyers, :experience, unique: true
+    add_index :lawyers, :rating, unique: true
   end
 end
