519481436484392a122c16e316a57c71d28f9a3c
Anush Veeranala <anush.2k10@gmail.com>
2014-04-06 15:58:09 +0530

    coded service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,9 @@
 class Service < ActiveRecord::Base
   # attr_accessible :title, :body
+
+  has_many :charges, inverse_of: :service, dependent: :destroy
+
+  validates :code, presence: true, length: {maximum: 250}, uniqueness: {case_sensitive: true}
+  validates :name, presence: true, length: {maximum: 250}
+
 end
