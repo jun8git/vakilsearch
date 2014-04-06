e4c4f098aac69cc144e48162c2ead3a488eecc54
Anush Veeranala <anush.2k10@gmail.com>
2014-04-06 17:00:17 +0530

    working on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,10 @@ class PagesController < ApplicationController
 
   def index
     @cities = City.find(:all, order: :name)
-    if @cities.map(&:name).include?(cookies[:city])
-      # @services =
+    @service_flag = @cities.map(&:name).include?(cookies[:city])
+    if @service_flag
+      city = City.where(name: cookies[:city])
+      @services = Service.city_services(city)
     end
   end
 
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,8 +2,14 @@ class Service < ActiveRecord::Base
   # attr_accessible :title, :body
 
   has_many :charges, inverse_of: :service, dependent: :destroy
+  has_many :lawyers, through: :charges
 
   validates :code, presence: true, length: {maximum: 250}, uniqueness: {case_sensitive: true}
   validates :name, presence: true, length: {maximum: 250}
+
+  scope :city_services, lambda{ |city|
+    joins(:lawyers).where("lawyers.city_id" => city)
+  }
+
 
 end
